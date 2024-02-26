1dbad8f920e773616ba768f5573c328ec75eff5c
Sheep1sik <bohg6799@naver.com>
2024-02-26 19:11:33 +0900

    Feat: NavigationBarView / LogoView 구현

--- Touchdown/Touchdown/App/ContentView.swift
+++ Touchdown/Touchdown/App/ContentView.swift
@@ -13,8 +13,29 @@ struct ContentView: View {
     // MARK: - BODY
     
     var body: some View {
-        FooterView()
-            .padding(.horizontal)
+        ZStack {
+            VStack(spacing: 0) {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.connectedScenes
+                                            .compactMap { $0 as? UIWindowScene }
+                                            .first?
+                                            .windows
+                                            .first?
+                                            .safeAreaInsets
+                                            .top ?? 0)
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+                    
+                Spacer()
+                
+                FooterView()
+                    .padding(.horizontal)
+            } //: VSTACK
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        } //: ZSTACK
+        .ignoresSafeArea(.all, edges: .top)
     }
 }
 
