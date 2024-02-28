7c54e3cebddac971691cb9b68de5d2f11bfa3b1a
Sheep1sik <bohg6799@naver.com>
2024-02-28 14:39:22 +0900

    Feat: BrandView 구현

---
+++ Touchdown/Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by 양원식 on 2024/02/27.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
---
+++ Touchdown/Touchdown/View/Home/BrandGridView.swift
@@ -0,0 +1,30 @@
+//
+//  BrandGridView.swift
+//  Touchdown
+//
+//  Created by 양원식 on 2024/02/28.
+//
+
+import SwiftUI
+
+struct BrandGridView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false, content: {
+            LazyHGrid(rows: gridLayout, spacing: columnSpacing, content: {
+                ForEach(brands) { brand in
+                    BrandItemView(brand: brand)
+                }
+            }) //: GRID
+            .frame(height: 200)
+            .padding(15)
+        }) //: SCROLL
+    }
+}
+
+struct BrandGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        BrandGridView()
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
+    }
+}
